d86b1ebbf381c7124a02d5dd8107ea3c675cfb57
Juan Covarrubias <me@juan.fi>
2024-01-28 16:01:58 +0200

    Add test parse_binary_op_with_int_and_identifier

--- Sources/Tests/ParserTests.swift
+++ Sources/Tests/ParserTests.swift
@@ -3,7 +3,7 @@ import XCTest
 @testable import swiftcompiler
 
 final class ParserTests: XCTestCase {
-  func test_a_parse_binary_op() throws {
+  func test_a_parse_binary_op_with_ints() throws {
     var tokenizer = Tokenizer(input: "1 + 2")
     tokenizer.tokenize()
     var parser = Parser(tokens: tokenizer.tokens)
@@ -19,6 +19,23 @@ final class ParserTests: XCTestCase {
       binaryOpExpression,
       BinaryOpExpression(
         left: LiteralExpression(value: 1), op: "+", right: LiteralExpression(value: 2)))
-    print(binaryOpExpression)
+  }
+
+  func test_b_parse_binary_op_with_int_and_identifier() throws {
+    var tokenizer = Tokenizer(input: "1 + a")
+    tokenizer.tokenize()
+    var parser = Parser(tokens: tokenizer.tokens)
+    let expression = parser.parseExpression()
+    guard
+      let binaryOpExpression = expression
+        as? BinaryOpExpression
+    else {
+      XCTFail("Expected BinaryOpExpression, got \(String(describing: expression))")
+      return
+    }
+    XCTAssertEqual(
+      binaryOpExpression,
+      BinaryOpExpression(
+        left: LiteralExpression(value: 1), op: "+", right: IdentifierExpression(value: "a")))
   }
 }
