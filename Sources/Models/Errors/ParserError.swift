d9691c46d61a4364a90d3a1b31caa7b00e3840f2
Juan Covarrubias <me@juan.fi>
2024-02-02 17:01:14 +0200

    Update ParserError to allow for an optional precedingToken

--- Sources/Models/Errors/ParserError.swift
+++ Sources/Models/Errors/ParserError.swift
@@ -1,7 +1,7 @@
 enum ParserError: Error, Equatable {
   case unexpectedTokenValue(token: Token, expected: [String])
   case unexpectedTokenType(token: Token, expected: [TokenType])
-  case noTokenFound(precedingToken: Token)
+  case noTokenFound(precedingToken: Token?)
   case ifExpressionMissingCondition
   case ifExpressionMissingThenExpression
 }
