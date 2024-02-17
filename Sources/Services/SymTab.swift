3ea14caa60cdfed26e8f37e97955fd51250e60e6
Juan Covarrubias <me@juan.fi>
2024-02-17 16:50:34 +0200

    Overload SymTab insert & lookup func

--- Sources/Services/SymTab.swift
+++ Sources/Services/SymTab.swift
@@ -9,7 +9,15 @@ struct SymTab<T> {
     table[key] = value
   }
 
+  mutating func insert<S: CustomStringConvertible>(_ value: T, for key: S) {
+    insert(value, for: key.description)
+  }
+
   func lookup(_ key: String) -> T? {
     return table[key]
+  }
+
+  func lookup<S: CustomStringConvertible>(_ key: S) -> T? {
+    return lookup(key.description)
   }
 }
