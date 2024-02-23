be6c4936ef5295a63a9b365692045e139f2b698b
Juan Covarrubias <me@juan.fi>
2024-02-23 11:47:07 +0200

    Fix main

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,13 +1,13 @@
 let input = """
-  var n: Bool = not true
-  print_bool(n)
+  var n: Bool = not true;
+  print_bool(n);
   """
 var tokenizer = Tokenizer(input: input)
 try tokenizer.tokenize()
 var parser = Parser(tokens: tokenizer.tokens)
 var typechecker = Typechecker()
-var expression = try parser.parse().compactMap { $0 }
-var typedExpressions = try expression.map { try typechecker.typecheck($0) }
-var irGenerator = try IrGenerator(expressions: typedExpressions)
+var expression = try parser.parse()
+var typedExpressions = try typechecker.typecheck(expression)
+var irGenerator = try IrGenerator(expressions: [typedExpressions])
 try irGenerator.generate()
 irGenerator.instructions.forEach { print($0) }
