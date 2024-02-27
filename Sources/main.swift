dba5b09ec529f8b058d21fe4758c081550904c83
Juan Covarrubias <me@juan.fi>
2024-02-27 14:53:06 +0200

    Testing in main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,12 +1,14 @@
 let input = """
-  var a = true or true;
+  var a = false or true
   """
 var tokenizer = Tokenizer(input: input)
 try tokenizer.tokenize()
 var parser = Parser(tokens: tokenizer.tokens)
 var typechecker = Typechecker()
 var expression = try parser.parse()
-var typedExpressions = try typechecker.typecheck(expression)
-var irGenerator = try IrGenerator(expressions: [typedExpressions])
+var typedExpression = try typechecker.typecheck(expression)
+var irGenerator = try IrGenerator(expression: typedExpression)
 try irGenerator.generate()
 irGenerator.instructions.forEach { print($0) }
+let irVariables = getIrVarsFromInstructions(irGenerator.instructions)
+irVariables.forEach { print($0) }
