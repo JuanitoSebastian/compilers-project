340b4a97fb9c8e474c04a0ce47535e2d1c74c637
Juan Covarrubias <me@juan.fi>
2024-02-27 13:39:17 +0200

    Add function to get IR variables from instructions

--- Sources/Utils/IrUtilities.swift
+++ Sources/Utils/IrUtilities.swift
@@ -5,3 +5,9 @@ let irBuiltInFuncs = [
 .reduce(into: [String: IrVar]()) { map, op in
   map[op] = IrVar(name: op)
 }
+
+/// Returns the IR variables used in the given instructions. Removes duplicates.
+func getIrVarsFromInstructions(_ instructions: [(any Instruction)]) -> [IrVar] {
+  let irVariables = Set(instructions.flatMap { $0.irVariables })
+  return Array(irVariables)
+}
