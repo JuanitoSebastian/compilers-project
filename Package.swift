de4bf0a99e9541c6e799d3b0a9a0281e80afe594
Juan Covarrubias <me@juan.fi>
2024-03-02 17:27:34 +0200

    Add ColorizeSwift as a dependency

--- Package.swift
+++ Package.swift
@@ -8,14 +8,18 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/realm/SwiftLint.git", from: "0.54.0"),
     .package(url: "https://github.com/apple/swift-format.git", from: "509.0.0"),
-    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.2.1"))
+    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.2.1")),
+    .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.5.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .executableTarget(
       name: "swiftcompiler",
-      dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")],
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "ColorizeSwift", package: "ColorizeSwift")
+      ],
       exclude: ["Tests"]
     ),
     .testTarget(
