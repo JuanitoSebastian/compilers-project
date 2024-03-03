a3c96d87ad3c705ccf256fa4582ca93053b48ad0
Juan Covarrubias <me@juan.fi>
2024-03-03 17:52:02 +0200

    Create first e2e tests

---
+++ Sources/Tests/SwiftCompilerTests.swift
@@ -0,0 +1,78 @@
+import XCTest
+
+@testable import swiftcompiler
+
+final class SwiftCompilerTests: XCTestCase {
+  func test_e2e_basic_maths() throws {
+    let inputCode = """
+      var x = 20 * 5;
+      var y = 4;
+      var result = x / y + (-10);
+      print_int(result);
+      var z = 348928 % 125
+      """
+    var test = try XCTUnwrap(SwiftCompiler.parseAsRoot([inputCode, "-o", "test"]))
+    try test.run()
+    XCTAssertTrue(checkFileExists("build/test"))
+    let output = try runProgramAndExpectOutput("./build/test")
+    XCTAssertEqual(output, ["15", "53"])
+  }
+
+  func test_e2e_while_loop() throws {
+    let inputCode = """
+      var x = 0;
+      while x < 10 do {
+        print_int(x);
+        x = x + 1;
+      }
+      """
+    var test = try XCTUnwrap(SwiftCompiler.parseAsRoot([inputCode, "-o", "test"]))
+    try test.run()
+    XCTAssertTrue(checkFileExists("build/test"))
+    let output = try runProgramAndExpectOutput("./build/test")
+    XCTAssertEqual(output, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+  }
+
+  func test_e2e_if_else() throws {
+    let inputCode = """
+      var x = 10;
+      if x <= 10 then {
+        print_int(1);
+      } else {
+        print_int(0);
+      }
+      print_int(28931)
+      """
+    var test = try XCTUnwrap(SwiftCompiler.parseAsRoot([inputCode, "-o", "test"]))
+    try test.run()
+    XCTAssertTrue(checkFileExists("build/test"))
+    let output = try runProgramAndExpectOutput("./build/test")
+    XCTAssertEqual(output, ["1", "28931"])
+  }
+}
+
+extension SwiftCompilerTests {
+  private func checkFileExists(_ path: String) -> Bool {
+    return FileManager.default.fileExists(atPath: path)
+  }
+
+  private func runProgramAndExpectOutput(_ program: String) throws -> [String] {
+    let task = Process()
+    let pipe = Pipe()
+
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", program]
+    task.executableURL = URL(fileURLWithPath: "/bin/bash")
+    task.standardInput = nil
+
+    try task.run()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)
+
+    guard let output = output else { return [] }
+
+    return output.split(separator: "\n").map(String.init)
+  }
+}
